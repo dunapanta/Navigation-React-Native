a470b965b73726854e551bb1a0430852ec4676e3
Daniel <dunapanta2@gmail.com>
2021-04-18 21:22:49 -0500

    Using useSafeAreaInsets for safearea

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export const SettingsScreen = () => {
+
+    const insets = useSafeAreaInsets()
+
     return (
-        <View>
+        <View  style={{ marginTop: insets.top}}>
            <Text>Settings Screen</Text> 
         </View>
     )
