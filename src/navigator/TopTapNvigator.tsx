45d1ba4b6272f1cdcb1c49f546dd977b17bdf46e
Daniel <dunapanta2@gmail.com>
2021-04-19 20:23:29 -0500

    Customizing Top Tab Navigator

--- src/navigator/TopTapNvigator.tsx
+++ src/navigator/TopTapNvigator.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { TopTabChatScreen } from '../screens/TopTabChatScreen';
-import { TopTabContactsScreen } from '../screens/TopTabContactsScreen';
-import { TopTabAlbums } from '../screens/TopTabAlbums';
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {TopTabChatScreen} from '../screens/TopTabChatScreen';
+import {TopTabContactsScreen} from '../screens/TopTabContactsScreen';
+import {TopTabAlbums} from '../screens/TopTabAlbums';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {colors} from '../theme/appTheme';
+import { Text } from 'react-native';
 
 /* import {LogBox} from 'react-native'
 LogBox.ignoreLogs(['Sending']) */
@@ -10,11 +13,56 @@ LogBox.ignoreLogs(['Sending']) */
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+  const {top} = useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{
+        paddingTop: top,
+      }}
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      tabBarOptions={{
+        pressColor: colors.primary,
+        showIcon: true,
+        indicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        style: {
+          //quitar linea
+          shadowColor: 'transparent', //ios
+          elevation: 0, // android
+        },
+      }}
+
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, focused }) => {
+          let iconName: string = '';
+
+          switch (route.name) {
+            case 'TopTabChatScreen':
+              iconName = 'Chat';
+              break;
+            case 'TopTabContactsScreen':
+              iconName = 'Contactos';
+              break;
+
+            case 'TopTabAlbums':
+              iconName = 'Albums';
+              break;
+          }
+
+          return <Text style={{color: colors.secondaryDark}}>{iconName}</Text>;
+        },
+      })}
+      >
       <Tab.Screen name="TopTabChatScreen" component={TopTabChatScreen} />
-      <Tab.Screen name="TopTabContactsScreen" component={TopTabContactsScreen} />
+      <Tab.Screen
+        name="TopTabContactsScreen"
+        component={TopTabContactsScreen}
+      />
       <Tab.Screen name="TopTabAlbums" component={TopTabAlbums} />
     </Tab.Navigator>
   );
-}
+};
