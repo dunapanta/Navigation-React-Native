1c5f3329fd1e52115b602b6c1b09d81008cd370c
Daniel <dunapanta2@gmail.com>
2021-04-19 20:51:39 -0500

    Adding icons on Android on build.gradle

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native';
+import React, {useEffect} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {View, Text} from 'react-native';
+
+import {styles} from '../theme/appTheme';
 
 export const Tab1Screen = () => {
+  useEffect(() => {
+    console.log('Tab 1 Screen Effect');
+  }, []);
 
-    useEffect(() => {
-        console.log('Tab 1 Screen Effect')
-    }, [])
-
-    return (
-        <View>
-            <Text>Tab 1 Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.globalMargin}>
+      <Text>Iconos</Text>
+      <Text>
+      <Icon name="cut-outline" size={100} color="#900" />
+      </Text>
+      
+    </View>
+  );
+};
